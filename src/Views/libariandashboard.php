844e2e7001988e6e281a7e2156fec3328a064d9e
Dasula <dasulaniduwara1980@gmail.com>
2026-02-24 17:10:10 +0530

    Add libarian dashboard and styles

---
+++ src/Views/libariandashboard.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../public/assets/css/dashboradstyle.css">
+</head>
+
+<div class="container-fluid">
+
+    <body>
+        <div class="row">
+            <div class="col-12-lg">
+                <h1 class="my-4">Dashboard</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="card hover-card" style="width: 18rem; margin: auto;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Total Members</h5>
+                        <h3 class="card-text text-center">53</h3>
+                        <a href="#" class="btn btn-primary d-block justify-content-center">Member Directory</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card hover-card" style="width: 18rem; margin: auto;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Active loans</h5>
+                        <h3 class="card-text text-center">1250</h3>
+                        <a href="#" class="btn btn-primary d-block justify-content-center">Book Management</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="card hover-card" style="width: 18rem; margin: auto;">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Total Guests</h5>
+                        <h3 class="card-text text-center">100</h3>
+                        <a href="#" class="btn btn-primary d-block justify-content-center">Guest Management</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+<div class="row">
+
+</div>
+</div>
+
+<script src="public/assets/libs/bootstrap-5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>

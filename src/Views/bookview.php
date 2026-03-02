ad11de769008067e051fccd972c07139db498482
Jayodya <dewminijdh@gmail.com>
2026-03-02 13:21:23 +0530

    search page

--- src/Views/bookview.php
+++ src/Views/bookview.php
@@ -1,3 +1,7 @@
+<?php
+    include 'src/Controllers/BookController.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- src/Views/reservebook.php
+++ src/Views/reservebook.php
@@ -1,3 +1,7 @@
+<?php
+    include '../Controllers/BookController.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
